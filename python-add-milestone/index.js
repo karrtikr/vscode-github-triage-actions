eb1fae0349004000db53e7a6b7849dd644a083c9
Kartik Raj <karraj@microsoft.com>
2023-09-06 12:55:39 -0700

    Add automation to add milestones

---
+++ python-add-milestone/index.js
@@ -0,0 +1,30 @@
+"use strict";
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const Action_1 = require("../common/Action");
+class PythonAddMilestoneAction extends Action_1.Action {
+    constructor() {
+        super(...arguments);
+        this.id = 'PythonAddMilestoneAction';
+    }
+    async onClosed(issue) {
+        await enrollIssue(issue);
+    }
+}
+const enrollIssue = async (issue) => {
+    var _a;
+    const closingHash = (_a = (await issue.getClosingInfo())) === null || _a === void 0 ? void 0 : _a.hash;
+    if (closingHash) {
+        // Get the milestone linked to the current release and set it if the issue doesn't have one
+        const releaseMilestone = (await issue.getIssue()).milestone
+            ? undefined
+            : await issue.getCurrentRepoMilestone();
+        if (releaseMilestone !== undefined) {
+            await issue.setMilestone(releaseMilestone);
+        }
+    }
+};
+//# sourceMappingURL=index.js.map
---
+++ python-add-milestone/index.ts
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { OctoKitIssue } from '../api/octokit';
+import { Action } from '../common/Action';
+import { GitHubIssue } from '../api/api';
+
+class PythonAddMilestoneAction extends Action {
+	id = 'PythonAddMilestoneAction';
+
+	async onClosed(issue: OctoKitIssue) {
+		await enrollIssue(issue);
+	}
+}
+
+const enrollIssue = async (issue: GitHubIssue) => {
+	const closingHash = (await issue.getClosingInfo())?.hash;
+	if (closingHash) {
+		// Get the milestone linked to the current release and set it if the issue doesn't have one
+		const releaseMilestone = (await issue.getIssue()).milestone
+			? undefined
+			: await issue.getCurrentRepoMilestone();
+		if (releaseMilestone !== undefined) {
+			await issue.setMilestone(releaseMilestone);
+		}
+	}
+};
